3fc13b8c8a9abec10e36bdf244046d26292f4e98
Saiguer10 <eljosemsp@gmail.com>
2025-05-26 16:24:28 +0200

    Añadidos archivos DTO en la rama Dto

---
+++ src/main/java/com/project_final/order_service/Dto/UserDto.java
@@ -0,0 +1,41 @@
+package com.project_final.order_service.Dto;
+
+public class UserDto {
+    private Long id;
+    private String name;
+    private String email;
+
+    // Constructores
+    public UserDTO() {}
+
+    public UserDTO(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
