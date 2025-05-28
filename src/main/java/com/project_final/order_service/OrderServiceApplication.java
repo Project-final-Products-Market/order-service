32b713d90fe49dcb7d247bc0ae3934deff760b49
Saiguer10 <eljosemsp@gmail.com>
2025-05-28 09:50:10 +0200

    Commit modificacion aplication ok

--- src/main/java/com/project_final/order_service/OrderServiceApplication.java
+++ src/main/java/com/project_final/order_service/OrderServiceApplication.java
@@ -3,7 +3,7 @@ package com.project_final.order_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced; // ← AÑADIR
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,7 +16,7 @@ public class OrderServiceApplication {
 	}
 
 	@Bean
-	@LoadBalanced  // ← AÑADIR ESTA ANOTACIÓN
+	@LoadBalanced
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
